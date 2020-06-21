1350f46b851b0ec366a5cad0f0a8a5a30c030d4f
Mark-Ball <markball111@gmail.com>
2020-06-22 06:37:59 +1000

    added data fetching with useEffect hook

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,41 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Order from '../Order/Order';
 
 // mocked object from rails app
-const response = [
-    {
-    "id": 1,
-    "agency": {
-        "id": 1,
-        "title": "Ray White Chatswood"
-    },
-    "status": {
-        "id": 2,
-        "title": "Approved"
-    },
-    "campaign": {
-        "id": 1,
-        "unit_number": 4,
-        "street_number": 221,
-        "street_name": "Pacific Highway",
-        "suburb_name": "Chatswood"
-    },
-    "title": "A4 Brochures",
-    "quantity": 100
-    }
-]
+// const response = [
+//     {
+//     "id": 1,
+//     "agency": {
+//         "id": 1,
+//         "title": "Ray White Chatswood"
+//     },
+//     "status": {
+//         "id": 2,
+//         "title": "Approved"
+//     },
+//     "campaign": {
+//         "id": 1,
+//         "unit_number": 4,
+//         "street_number": 221,
+//         "street_name": "Pacific Highway",
+//         "suburb_name": "Chatswood"
+//     },
+//     "title": "A4 Brochures",
+//     "quantity": 100
+//     }
+// ]
 const Orders = () => {
-    // useEffect(async () => {
-    //     const r = await axios.get('http://localhost:3000/orders');
-    //     console.log(r);
-    //     // also need to fix CORS in rails app
-    // })
+    const [data, setData] = useState([]);
+    
+    useEffect(() => {
+        const getOrders = async () => {
+            const result = await axios.get('http://localhost:3000/orders');
+            setData(result.data);
+        }
+        getOrders();
+    }, []);
 
     return (
         <>
             <h1>Orders</h1>
-            {response.map(order => (
+            {data.map(order => (
                 <Order
                     key={order.id}
                     id={order.id}
